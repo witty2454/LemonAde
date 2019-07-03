e58fd9090b36f1045cf9e67d587a0611cad70cbc
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-07-03 07:33:20 -0500

    added a supply and Cup class, and there assocsiated methods and varibles.

---
+++ Supply.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lemonadestand
+{
+    public abstract class Supply
+    {
+        public string name;
+        public double price;
+
+        public Supply()
+        {
+
+        }
+    }
+}
